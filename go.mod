88e59f29abaea52d834ccc9f7827ac8bd364410a
eulersexception <bahadir.suezer@outlook.com>
2021-02-18 00:51:44 +0100

    first steps to embedded db

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eulersexception/glabs-ui
 
 go 1.14
 
-require fyne.io/fyne v1.3.3
+require (
+	fyne.io/fyne v1.3.3
+	github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	glabsView "github.com/eulersexception/glabs-ui/view"
+	"fmt"
+	"log"
+
+	"github.com/dgraph-io/badger/v3"
 )
 
 func main() {
 
-	glabsView.NewHomeview().Window.ShowAndRun()
+	db, err := badger.Open(badger.DefaultOptions("tmp/badger"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	k := "Bahadir"
+	v := "Suezer"
+	updates := make(map[string]string)
+	updates[k] = v
+
+	err = db.Update(func(txn *badger.Txn) error {
+		e := badger.NewEntry([]byte(k), []byte(v))
+		err = txn.SetEntry(e)
+		return err
+	})
+
+	err = db.View(func(txn *badger.Txn) error {
+		item, getErr := txn.Get([]byte(k))
+
+		if getErr != nil {
+			log.Fatal(getErr)
+		}
+
+		err = item.Value(func(val []byte) error {
+			fmt.Println(fmt.Sprintf("Key = %s, Value = %s", item.String(), string(val)))
+			return err
+		})
+		return getErr
+	})
+
+	//glabsView.NewHomeview().Window.ShowAndRun()
 
 }
